224f81829f2085915206f4c9e9dbf6999d9af782
jahid <info.jahidulislamjahid@gmail.com>
2021-09-04 14:46:13 +0600

    showing the weather results

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 const apiKey = `d7cd31b067ecef28a1e95426431eec8e`;
 const callWeather = () => {
     const areaInput = document.getElementById('input-field').value;
-    const api = `https://api.openweathermap.org/data/2.5/weather?q=${areaInput}&appid=${apiKey}`;
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${areaInput}&appid=${apiKey}&units=metric`;
+    console.log(api);
     fetch(api)
         .then(res => res.json())
         .then(data => displayTemp(data));
 }
 const displayTemp = temperature => {
     console.log(temperature);
+    document.getElementById('city-name').innerText = temperature.name;
+    document.getElementById('units').innerText = temperature.main.temp;
+    document.getElementById('condition').innerText = temperature.weather[0].main;
 }
