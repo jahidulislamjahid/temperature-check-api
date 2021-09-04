5d3a22671cd077389926a9fa63d3b747ad88b670
jahid <info.jahidulislamjahid@gmail.com>
2021-09-04 15:11:58 +0600

    finalization

--- js/app.js
+++ js/app.js
@@ -6,10 +6,13 @@ const callWeather = () => {
     fetch(api)
         .then(res => res.json())
         .then(data => displayTemp(data));
+    document.getElementById('input-field').value = '';
 }
 const displayTemp = temperature => {
     console.log(temperature);
     document.getElementById('city-name').innerText = temperature.name;
     document.getElementById('units').innerText = temperature.main.temp;
     document.getElementById('condition').innerText = temperature.weather[0].main;
+    let icons = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`
+    document.getElementById('icon').setAttribute('src', icons);
 }
